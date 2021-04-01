542d94a4f818e373439dd597e40362629ad691e5
pratikal1 <chandpratikal1@gmail.com>
2021-04-01 21:54:20 +0530

    added authentication and functionality to login and register pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,52 @@
 import React from 'react';
-
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react';
+import PostCard from '../components/PostCard';
 export default function Home() {
-  return <div>Home</div>;
+  const { loading, data: { getPosts: posts } = {} } = useQuery(
+    FETCH_POSTS_QUERY
+  );
+
+  return (
+    <Grid columns={3}>
+      <Grid.Row className="page-title">
+        <h1>Recent Quotes</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? (
+          <h1>Loading posts..</h1>
+        ) : (
+          posts &&
+          posts.map((post) => (
+            <Grid.Column key={post.id} style={{ marginBottom: 20 }}>
+              <PostCard post={post} />
+            </Grid.Column>
+          ))
+        )}
+      </Grid.Row>
+    </Grid>
+  );
 }
+
+const FETCH_POSTS_QUERY = gql`
+  {
+    getPosts {
+      id
+      body
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      commentCount
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+    }
+  }
+`;
